ab206028b3f117956ab08a3f508eaa6976702244
RevisOTO <rafamosqueda.barragan12@gmail.com>
2020-10-08 23:10:33 -0500

    Commit 2 8/Oct/20 11:10      Primer test

--- src/Puzzle/Jueguito.java
+++ src/Puzzle/Jueguito.java
@@ -2,21 +2,19 @@ package Puzzle;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
 import java.awt.FlowLayout;
 import javax.swing.JTextField;
 import java.awt.GridLayout;
 import java.awt.CardLayout;
 import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
+import java.util.Random;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Jueguito extends JFrame {
 
@@ -49,6 +47,7 @@ public class Jueguito extends JFrame {
 		setContentPane(contentPane);
 		SpringLayout sl_contentPane = new SpringLayout();
 		contentPane.setLayout(sl_contentPane);
+		 
 		
 		JButton btnButton1 = new JButton("1");
 		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1, 15, SpringLayout.NORTH, contentPane);
@@ -155,39 +154,109 @@ public class Jueguito extends JFrame {
 		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton15, 0, SpringLayout.EAST, btnButton5);
 		contentPane.add(btnButton15);
 		
-		JButton btnButton1_1 = new JButton("1");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1_1, 6, SpringLayout.SOUTH, btnButton11);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton1_1, 0, SpringLayout.WEST, btnButton1);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton1_1, 72, SpringLayout.SOUTH, btnButton11);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton1_1, 0, SpringLayout.EAST, btnButton1);
-		contentPane.add(btnButton1_1);
+		JButton btnButton16 = new JButton("16");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton16, 6, SpringLayout.SOUTH, btnButton11);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton16, 0, SpringLayout.WEST, btnButton1);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton16, 72, SpringLayout.SOUTH, btnButton11);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton16, 0, SpringLayout.EAST, btnButton1);
+		contentPane.add(btnButton16);
 		
-		JButton btnButton1_2 = new JButton("1");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1_2, 6, SpringLayout.SOUTH, btnButton12);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton1_2, 0, SpringLayout.WEST, btnButton2);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton1_2, 0, SpringLayout.SOUTH, btnButton1_1);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton1_2, 0, SpringLayout.EAST, btnButton2);
-		contentPane.add(btnButton1_2);
+		JButton btnButton17 = new JButton("17");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton17, 6, SpringLayout.SOUTH, btnButton12);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton17, 0, SpringLayout.WEST, btnButton2);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton17, 0, SpringLayout.SOUTH, btnButton16);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton17, 0, SpringLayout.EAST, btnButton2);
+		contentPane.add(btnButton17);
 		
-		JButton btnButton1_3 = new JButton("1");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1_3, 6, SpringLayout.SOUTH, btnButton13);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton1_3, 0, SpringLayout.WEST, btnButton3);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton1_3, 0, SpringLayout.SOUTH, btnButton1_1);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton1_3, 0, SpringLayout.EAST, btnButton3);
-		contentPane.add(btnButton1_3);
+		JButton btnButton18 = new JButton("18");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton18, 6, SpringLayout.SOUTH, btnButton13);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton18, 0, SpringLayout.WEST, btnButton3);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton18, 0, SpringLayout.SOUTH, btnButton16);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton18, 0, SpringLayout.EAST, btnButton3);
+		contentPane.add(btnButton18);
 		
-		JButton btnButton1_4 = new JButton("1");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1_4, 6, SpringLayout.SOUTH, btnButton14);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton1_4, 0, SpringLayout.WEST, btnButton4);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton1_4, 0, SpringLayout.SOUTH, btnButton1_1);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton1_4, 0, SpringLayout.EAST, btnButton4);
-		contentPane.add(btnButton1_4);
+		JButton btnButton19 = new JButton("19");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton19, 6, SpringLayout.SOUTH, btnButton14);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton19, 0, SpringLayout.WEST, btnButton4);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton19, 0, SpringLayout.SOUTH, btnButton16);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton19, 0, SpringLayout.EAST, btnButton4);
+		contentPane.add(btnButton19);
 		
-		JButton btnButton1_5 = new JButton("1");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton1_5, 6, SpringLayout.SOUTH, btnButton15);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton1_5, 0, SpringLayout.WEST, btnButton5);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton1_5, 72, SpringLayout.SOUTH, btnButton15);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton1_5, 0, SpringLayout.EAST, btnButton5);
-		contentPane.add(btnButton1_5);
+		JButton btnButton20 = new JButton("20");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton20, 6, SpringLayout.SOUTH, btnButton15);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton20, 0, SpringLayout.WEST, btnButton5);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton20, 72, SpringLayout.SOUTH, btnButton15);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton20, 0, SpringLayout.EAST, btnButton5);
+		contentPane.add(btnButton20);
+		
+		JButton btnButton21 = new JButton("21");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton21, 6, SpringLayout.SOUTH, btnButton16);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton21, 0, SpringLayout.WEST, btnButton1);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton21, 72, SpringLayout.SOUTH, btnButton16);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton21, 0, SpringLayout.EAST, btnButton1);
+		contentPane.add(btnButton21);
+		
+		JButton btnButton22 = new JButton("22");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton22, 6, SpringLayout.SOUTH, btnButton17);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton22, 0, SpringLayout.WEST, btnButton2);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton22, 0, SpringLayout.SOUTH, btnButton21);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton22, 0, SpringLayout.EAST, btnButton2);
+		contentPane.add(btnButton22);
+		
+		JButton btnButton23 = new JButton("23");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton23, 6, SpringLayout.SOUTH, btnButton18);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton23, 0, SpringLayout.WEST, btnButton3);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton23, 0, SpringLayout.SOUTH, btnButton21);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton23, 0, SpringLayout.EAST, btnButton3);
+		contentPane.add(btnButton23);
+		
+		JButton btnButton24 = new JButton("24");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton24, 6, SpringLayout.SOUTH, btnButton19);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton24, 0, SpringLayout.WEST, btnButton4);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton24, 0, SpringLayout.SOUTH, btnButton21);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton24, 0, SpringLayout.EAST, btnButton4);
+		contentPane.add(btnButton24);
+		
+		JButton btnButton25 = new JButton("");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnButton25, 6, SpringLayout.SOUTH, btnButton20);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnButton25, 0, SpringLayout.WEST, btnButton5);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnButton25, 0, SpringLayout.SOUTH, btnButton21);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnButton25, 0, SpringLayout.EAST, btnButton5);
+		contentPane.add(btnButton25);
+		
+		JButton btnIniciar = new JButton("Jugar");
+		btnIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for(int x = 0; x==24; x++)
+				{
+					JButton BotonAuxiliar = new JButton();
+					ArrayJButtons[x] = BotonAuxiliar;
+				}
+				
+			}
+		});
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnIniciar, 6, SpringLayout.SOUTH, btnButton22);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnIniciar, 139, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnIniciar, 29, SpringLayout.EAST, btnButton3);
+		contentPane.add(btnIniciar);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnSalir, 6, SpringLayout.SOUTH, btnIniciar);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnSalir, 0, SpringLayout.WEST, btnButton3);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnSalir, -6, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnSalir, -188, SpringLayout.EAST, contentPane);
+		contentPane.add(btnSalir);
+		public JButton[] ArrayJButtons = {btnButton1,btnButton2,btnButton3,btnButton4,btnButton5,btnButton6,btnButton7,btnButton8,btnButton9,btnButton10,btnButton11,btnButton12,btnButton13,btnButton14,btnButton15,btnButton16,btnButton17,btnButton18,btnButton19,btnButton20,btnButton21,btnButton22,btnButton23,btnButton24,btnButton25};
 	}
+	public int RandomNum()
+	{
+		Random Alazar = new Random();
+		int upperbound = 25;
+		return Alazar.nextInt(upperbound);
+	}
+	
 }
